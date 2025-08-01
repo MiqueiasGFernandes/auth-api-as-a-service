9573005ba65b4036e9ecf093018022124d146df6
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2025-08-01 13:33:28 -0300

    feat: add boostrap layer

---
+++ src/bootstrap/bootstrap.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { PresentationModule } from "@presentation/presentation.module";
+
+@Module({
+    imports: [
+        PresentationModule,
+    ]
+})
+export class BootstrapModule { }
