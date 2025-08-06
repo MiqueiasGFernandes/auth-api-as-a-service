2be56b5c7f05cb04f49bc15e09ba961a2725ceac
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2025-08-06 15:12:05 -0300

    feat: add max length validation

--- src/application/dto/user-input.dto.ts
+++ src/application/dto/user-input.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty, IsString } from "class-validator"
+import { IsNotEmpty, IsString, MaxLength } from "class-validator"
 
 export class UserInputDto {
+    @MaxLength(100)
     @IsString()
     @IsNotEmpty()
     username: string
 
+    @MaxLength(100)
     @IsString()
     @IsNotEmpty()
     password: string
